6fe600aa48873b26aec85c8b2402786c6c982d01
Dave Rintoul <drintoul@gmail.com>
2023-12-18 14:56:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,17 @@ import pandas as pd
 
 def main():
 
-  dfs = pd.read_html('https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates/')
-  df = dfs[0]
-  st.write(dfs)
-  st.dataframe(df)
+  #dfs = pd.read_html('https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates/')
+  #df = dfs[0]
+  #st.write(dfs)
+  #st.dataframe(df)
 
-  #st.toggle('Enter value', ['On', 'Off'])
+  cdn = st.toggle('Canadian Dollars')
+
+  if cdn:
+     	st.write('Canadian Dollars')
+  else:
+	st.write('U.S. Dollars')
 
 if __name__ == '__main__':
 	main()	
