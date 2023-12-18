8527b7b025bc98f9113a9721055fbed7559c4755
Dave Rintoul <drintoul@gmail.com>
2023-12-18 14:48:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import pandas as pd
 def main():
 
   dfs = pd.read_hmtl('https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates/')
-  df = dfs[0]
-
-  st.dataframe(df)
+  #df = dfs[0]
+  st.write(dfs)
+  #st.dataframe(df)
 
 if __name__ == '__main__':
 	main()	
