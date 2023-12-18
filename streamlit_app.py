4b13673f1b19f91b5089c6556f8e96452ee5cadd
Dave Rintoul <drintoul@gmail.com>
2023-12-18 15:19:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,21 @@ def main():
 	st.write(c2u, u2c)
 
 	cdn = st.toggle("Canadian Dollars")
+
+	amount = st.number_input("Enter Amount")
 	
 	if cdn:
+		sCurrency, tCurrency = "Canadian", "American"
+		sUnits, tUnits = "\$/Litre", "\$/gallon"
 		st.write(f"Today's Exchange rate is \$CDN {c2u} = \$US 1.00")
-		price = st.number_input("Enter today's gas price in \$CDN")
+		price = st.number_input("Enter gas price in \$CDN")
 	else:
+		sCurrency, tCurrency = "American", "Canadian"
+		sUnits, tUnits = "\$/gallon", "\$/Litre"
 		st.write(f"Today's Exchange rate is \$CDN 1.00 = \$US {u2c}")
-		price = st.number_input("Enter today's gas price in \$US")
+		price = st.number_input("Enter gas price in \$US")
 
-	st.write(price)
+	st.write(f"{amount} {sUnits} at {c2u} {sCurrency} dollars would cost {convert} {tUnits} {tCurrency}")
 
 if __name__ == '__main__':
 	main()	
