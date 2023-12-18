31b0b01934049636d50464655bb6934dec3ed092
Dave Rintoul <drintoul@gmail.com>
2023-12-18 16:15:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def main():
 
 	cost = round(amount * rate, 2)
 
-	st.write(f"{amount} {sUnits}s at {rate} \${price}/{sUnits} would cost \${cost} {tCurrency} dollars")
+	st.write(f"{amount} {sUnits}s at \${price}/{sUnits} would cost \${cost} {tCurrency} dollars")
 
 if __name__ == '__main__':
 	main()	
