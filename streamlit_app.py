2f0964f6e8a28accf840f028ac6919e1acb321c5
Dave Rintoul <drintoul@gmail.com>
2023-12-18 15:00:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,17 @@ import pandas as pd
 
 def main():
 
+	st.header('Canada/US Gas Price Comparison')
+	st.subheader('Compare gas prices including currency exchange')
+	
 	exchange = 1.37
 
 	cdn = st.toggle('Canadian Dollars')
 
 	if cdn:
-		st.write('Canadian Dollars')
+		st.write("Today's Exchange rate is $CDN {exchange} = $US 1.00")
 	else:
-		st.write('U.S. Dollars')
+	st.write("Today's Exchange rate is $CDN 1.00 == $US {1/exchange:1.2f}")
 
 if __name__ == '__main__':
 	main()	
