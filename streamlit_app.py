9d740e8831810bf83ce76aa3076e85728fdd3b79
Dave Rintoul <drintoul@gmail.com>
2023-12-19 09:18:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def main():
 		
 	cost =  price * amount * rate
 
-	st.info(f"""{amount:.1f} {sUnits}s ({convert:.1f} {tUnits}s) at \${price:.2f}/{sUnits} with an exchange rate of \${rate:.2f} 
+	st.info(f"""{amount:.1f} {sUnits}s ({convert:.1f} {tUnits}s) at {sCurrency} \${price:.2f}/{sUnits} with an exchange rate of {sCurrency} \${rate:.2f} = {tCurrency} \$1.00 
  		would cost \${cost:.2f} {tCurrency} (\${cost/rate:.2f} {sCurrency})""", icon="💰")
 
 if __name__ == '__main__':
