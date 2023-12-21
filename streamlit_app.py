7830dfc4a13190f8501c4b27e1604f647799cdca
Dave Rintoul <drintoul@gmail.com>
2023-12-21 06:38:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,30 +5,32 @@ st.set_page_config(page_title="Canada/US Gas Price Comparison")
 st.title('Canada/US Gas Price Comparison')
 st.subheader('Calculate gas prices including currency exchange')
 
-@st.cache_data(ttl=3600*6)
-def fetch_exchange_rate():
+def main():
 
-	dfs = pd.read_html("https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates")
-	df = dfs[0]
-	df = df[df['Currency'] == 'US dollar']
-	asof = df.columns[-1]
-	return df, asof
+	@st.cache_data(ttl=3600*6)
+	def fetch_exchange_rate():
 
-df, asof = fetch_exchange_rate()
+		dfs = pd.read_html("https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates")
+		df = dfs[0]
+		df = df[df['Currency'] == 'US dollar']
+		asof = df.columns[-1]
+		return df, asof
 
-cdn = st.toggle("Start with Canadian Dollars", value=True)
+	df, asof = fetch_exchange_rate()
 
-usd2cdn = df.iloc[-1, -1]
-cdn2usd = round(1 / usd2cdn, 4)
+	cdn = st.toggle("Start with Canadian Dollars", value=True)
 
-st.write("""Exchange Rate from Bank of Canada:
-https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates""")
+	usd2cdn = df.iloc[-1, -1]
+	cdn2usd = round(1 / usd2cdn, 4)
+
+	st.write("""Exchange Rate from Bank of Canada:
+	https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates""")
 
 #st.dataframe(df, hide_index=True)
 
 #st.divider()
 
-def main():
+#def main():
 
 	if cdn:
 		rate = cdn2usd 
